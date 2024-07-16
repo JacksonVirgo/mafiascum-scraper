34926d1c295b3f6a8800b090b73215104bba8905
Jack Nash <jacknash42@gmail.com>
2024-07-17 09:16:03 +1000

    chore: set up simple web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Get PORT from env or default to 3000
+    let port = std::env::var("PORT")
+        .unwrap_or("3000".to_string())
+        .parse::<u16>()
+        .expect("env.PORT must be an integer");
+
+    let address = format!("127.0.0.1:{}", port);
+    println!("Listening on {}", address);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(&address)?
+    .run()
+    .await
 }
