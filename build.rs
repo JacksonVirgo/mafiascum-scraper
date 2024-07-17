00fb5bf63d6bdff05a6ac2a6b160f7ed068c1a8f
Jack Nash <jacknash42@gmail.com>
2024-07-18 07:33:43 +1000

    revert: remove debug scripts in build.rs

--- build.rs
+++ build.rs
@@ -1,21 +1,4 @@
-use std::env;
-use std::fs;
-use std::path::{Path, PathBuf};
 use std::process::Command;
-
-fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> std::io::Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src)? {
-        let entry = entry?;
-        let file_type = entry.file_type()?;
-        if file_type.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        } else {
-            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        }
-    }
-    Ok(())
-}
 
 fn main() {
     Command::new("npm")
@@ -37,8 +20,4 @@ fn main() {
     if !output.status.success() {
         eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
     }
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_dir = PathBuf::from(out_dir).join("static");
-    copy_dir_all("static", &dest_dir).unwrap();
 }
