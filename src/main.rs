22c186c4cb464857376045bb2e83f0d290ae57d4
Jack Nash <jacknash42@gmail.com>
2024-07-18 07:48:00 +1000

    debug: test alternative css endpoint option

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use actix_files as fs;
+use actix_web::{get, HttpResponse, Responder};
 use actix_web::{web, App, HttpServer};
+use mime;
 mod routes;
 mod templates;
+
+const STYLE_CSS: &[u8] = include_bytes!("./static/output.css");
+#[get("/style.css")]
+async fn serve_css() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(mime::TEXT_CSS_UTF_8)
+        .body(STYLE_CSS)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -16,6 +26,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(fs::Files::new("/static", "./").show_files_listing())
+            .service(serve_css)
             .service(routes::main::main)
             .default_service(web::route().to(routes::not_found::not_found))
     })
